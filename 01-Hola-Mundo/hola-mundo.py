0e7859690fca07ef1a3509890e9b4fe9e257d1a6
Dilmer-R <rojascastaneda893@gmail.com>
2022-05-04 18:01:10 -0500

    TK

---
+++ 01-Hola-Mundo/hola-mundo.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+root = tk.Tk()
+m = tk.Label(root, text="Hola Mundo")
+m.pack()
+root.mainloop()
---
+++ 02-Ventanas/main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Cambiar título de ventana
+root = tk.Tk()
+root.title("Nombre ventana")
+# Tamaño de la ventana
+root.geometry("600x400+50+50")
+
+root.mainloop()
